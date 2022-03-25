9bb13f9c4283ec5558376b1753aba4a2c437aea5
Vi Phung <vi.phung179@gmail.com>
2022-03-25 16:43:44 -0400

    parse and query completed

--- app/src/main/java/com/vphung/codepathinsta/MainActivity.kt
+++ app/src/main/java/com/vphung/codepathinsta/MainActivity.kt
@@ -2,10 +2,41 @@ package com.vphung.codepathinsta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        queryPosts()
     }
+
+    fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post:" + post.getDescription() + " , username:" + post.getUser()?.username)
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
 }
--- app/src/main/java/com/vphung/codepathinsta/ParstagramApplication.kt
+++ app/src/main/java/com/vphung/codepathinsta/ParstagramApplication.kt
@@ -2,11 +2,15 @@ package com.vphung.codepathinsta
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParstagramApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        ParseObject.registerSubclass(Post::class.java)
+
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
