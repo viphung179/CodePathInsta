01e9adf7ceb7c965fc3452afc0945086a35be00f
Vi Phung <vi.phung179@gmail.com>
2022-03-30 21:59:56 -0400

    individual item layout built

--- app/src/main/java/com/vphung/codepathinsta/fragments/FeedFragment.kt
+++ app/src/main/java/com/vphung/codepathinsta/fragments/FeedFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.vphung.codepathinsta.R
 
 class FeedFragment : Fragment() {
+
+    lateinit var postsRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +18,12 @@ class FeedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        postsRecyclerView = view.findViewById(R.id.postRecyclerView)
     }
 
 }
